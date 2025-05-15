b7f13aeb8ce37413c8eced8a88046ba3e2fa4ae4
Prabhmeet Singh <prabhmeetmail@gmail.com>
2025-05-15 15:13:37 +0530

    Update 2. BellmanFord.cpp

--- 01. Graphs/08. Shortest Paths Algo/2. BellmanFord.cpp
+++ 01. Graphs/08. Shortest Paths Algo/2. BellmanFord.cpp
@@ -10,6 +10,37 @@
 
 class Solution {
 public:
+    vector<int> bellmanFord(int V, vector<vector<int>> &edges, int src)
+    {
+        const int INF = 1e9;
+        vector<int> dist(V, INF);
+        dist[src] = 0;
+    
+        // Relax all edges V - 1 times
+        for (int i = 1; i < V; ++i)
+        {
+            for (auto &edge : edges)
+            {
+                int u = edge[0], v = edge[1], w = edge[2];
+                if (dist[u] != INF && dist[u] + w < dist[v])
+                {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+        // Check for negative weight cycle
+        for (auto &edge : edges)
+        {
+            int u = edge[0], v = edge[1], w = edge[2];
+            if (dist[u] != INF && dist[u] + w < dist[v])
+            {
+                return {-1}; // Negative weight cycle detected
+            }
+        }
+    
+        return dist;
+    }
+
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> dis(n+1,6000);
         dis[k]=0;
