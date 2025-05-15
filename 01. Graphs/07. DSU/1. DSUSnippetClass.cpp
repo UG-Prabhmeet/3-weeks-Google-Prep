1c47dc5d1e358003639635616bac9d963ce1ca54
Prabhmeet Singh <prabhmeetmail@gmail.com>
2025-05-15 19:56:34 +0530

    Update 1. DSUSnippetClass.cpp

--- 01. Graphs/07. DSU/1. DSUSnippetClass.cpp
+++ 01. Graphs/07. DSU/1. DSUSnippetClass.cpp
@@ -23,6 +23,11 @@ class UnionFind {
         ele1 = find(ele1);
         ele2 = find(ele2);
         if(ele1 == ele2) return ;
+        // in union by ranks --> ranks of parents aren't increased
+        // as ranks are like the upper bounds of tree's height
+        // whereas in union by size --> then ranks are increased
+
+        // this is union by size 
         if(rank[ele1] > rank[ele2]) {
             parent[ele2] = ele1;
             rank[ele1] += rank[ele2];
@@ -30,5 +35,18 @@ class UnionFind {
             parent[ele1] = ele2;
             rank[ele2] += rank[ele1];
         }
-    }
+
+        // this is union by ranks 
+        if (rank[u] < rank[v]) {
+                parent[u] = v;
+            }
+            else if (rank[v] < rank[u]) {
+                parent[v] = u;
+            }
+            else {
+                parent[v] = u;
+                rank[u]++;    // only here, to reflect increased height
+            }
+            
+        }
 };
