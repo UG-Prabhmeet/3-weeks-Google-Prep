f026b81c14b692c9ecc8ad76897cdc9ada28257b
Prabhmeet Singh <prabhmeetmail@gmail.com>
2025-05-15 20:07:35 +0530

    Update 1. Kosarju.cpp

--- 01. Graphs/09. StronglyConnected/1. Kosarju.cpp
+++ 01. Graphs/09. StronglyConnected/1. Kosarju.cpp
@@ -8,48 +8,72 @@
 
 */
 
+#include <bits/stdc++.h>
+using namespace std;
 
-class Solution
+void topoStackDFS(int node,
+                  stack<int> &st,
+                  vector<bool> &visited,
+                  const vector<vector<int>> &adj)
 {
-	public:
-	//Function to find number of strongly connected components in the graph.
-	void dfs(int node,vector<int> adj[],vector<int>&visited,vector<int>&topo) {
-	    visited[node]=1;
-	    for(auto a:adj[node]) {
-		if(visited[a]==0)
-		dfs(a,adj,visited,topo);
-	    }
-	    topo.push_back(node);
-	}
-	
-    int kosaraju(int V, vector<int> adj[])
-    {
-        vector<int> topo;
-        vector<int> visited(V);
-        vector<int> revAdj[V];
-        
-        for(int i=0;i<V;i++) {
-            for(auto a:adj[i]) {
-                revAdj[a].push_back(i);
-            }
+    visited[node] = true;
+    for (int nbr : adj[node]) {
+        if (!visited[nbr]) {
+            topoStackDFS(nbr, st, visited, adj);
         }
-        for(int i=0;i<V;i++) {
-            if(visited[i]==0) {
-                dfs(i,adj,visited,topo);
-            }
-        }
-        reverse(topo.begin(),topo.end());
-        //for(auto a:topo)cout<<a<<" ";
-        vector<int> temp;
-        int ans=0;
-        for(int i=0;i<V;i++)visited[i]=0;
-        for(auto i:topo) {
-            if(visited[i]==0) {
-                dfs(i,revAdj,visited,temp);
-                ans++;
-            }
-        }
-        return ans;
-        
     }
-};
+    st.push(node);
+}
+
+// DFS on the transposed graph
+void revDFS(int node,
+            vector<bool> &visited,
+            const vector<vector<int>> &transposed)
+{
+    visited[node] = true;
+    for (int nbr : transposed[node]) {
+        if (!visited[nbr]) {
+            revDFS(nbr, visited, transposed);
+        }
+    }
+}
+
+// Kosaraju’s algorithm: returns number of strongly connected components
+int stronglyConnectedComp(int n, const vector<vector<int>> &edges) {
+    // Build directed adjacency list
+    vector<vector<int>> adj(n);
+    for (const auto &e : edges) {
+        int u = e[0], v = e[1];
+        adj[u].push_back(v);
+    }
+
+    // Fill vertices in stack according to finishing times
+    stack<int> st;
+    vector<bool> visited(n, false);
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            topoStackDFS(i, st, visited, adj);
+        }
+    }
+
+    // Transpose the graph
+    vector<vector<int>> transposed(n);
+    for (int u = 0; u < n; u++) {
+        for (int v : adj[u]) {
+            transposed[v].push_back(u);
+        }
+    }
+
+    // Process all vertices in order defined by the stack
+    fill(visited.begin(), visited.end(), false);
+    int sccCount = 0;
+    while (!st.empty()) {
+        int node = st.top();
+        st.pop();
+        if (!visited[node]) {
+            sccCount++;
+            revDFS(node, visited, transposed);
+        }
+    }
+    return sccCount;
+}
